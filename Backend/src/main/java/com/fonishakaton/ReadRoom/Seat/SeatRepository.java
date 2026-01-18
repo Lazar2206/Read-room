46eaf502a77ff2a63366ea2e8d122ccd26025acc
aleksa.veljic <av20250138@student.fon.bg.ac.rs>
2026-01-18 07:00:11 +0100

    backend

--- Backend/src/main/java/com/fonishakaton/ReadRoom/Seat/SeatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fonishakaton.ReadRoom.Seat;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface SeatRepository extends MongoRepository<Seat,String> {
-List<Seat> findByRoomId(String roomId);
-}
--- Backend/src/main/java/com/fonishakaton/ReadRoom/User/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fonishakaton.ReadRoom.User;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByEmail(String email);
-}
